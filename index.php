d51a17c80fdf5d121db2de84ef98282930fdde08
yogi736 <113496123+yogi736@users.noreply.github.com>
2022-09-14 14:01:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<!DOCKTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login Form</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+  
+<!-- <h2>Login Form </h2> -->
+<form action="tampil.php" method="POST">
+  <h2>Form Login </h2>
+  <div class="imgcontainer">
+    <img src="icon.png" alt="Avatar" class="Avatar">
+  </div>
+  
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placehole="Masukan Username" name="username " required>
+    
+    <label><b>password</b></label>
+    <input type="password" placeholder="Masukan Password" name="email" required>
+  
+    <button type="submit">Masuk</button>
+  <input type="checkbox" checked="checked"><span>Ingat Saya</span>
+  </div>
+  
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">batal</button>
+    <span class="psw">Lupa <a href="#">password?</a></span>
+  </div>
+  </form>
+  </body>
+  <?html>
+  
+
